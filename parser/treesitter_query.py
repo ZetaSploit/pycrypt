ef9d058ce3eee2bbfd1fd4bf14882ca4e07ca919
ZetaSploit <lazulilapis852@gmail.com>
2026-06-02 13:42:29 +0530

    treesitter_query.py now accepts user input

--- parser/treesitter_query.py
+++ parser/treesitter_query.py
@@ -4,34 +4,35 @@ from pathlib import Path
 
 
 PY_LANGUAGE = Language(tspython.language())
-
+ftp = input("File to be parsed: ")
 
 class FileParser:
     def __init__(self):
         self.parser = Parser(PY_LANGUAGE)
 
-    def read_file(self, path: str) -> str:
-        file_path = Path(path)
+    def read_file(self, ftp) -> str:
+        file_path = Path(ftp)
 
         if not file_path.exists():
-            raise FileNotFoundError(f"File not found: {path}")
+            raise FileNotFoundError(f"File not found: {ftp}")
 
         return file_path.read_text(encoding="utf-8")
 
     def parse_source(self, source: str):
         return self.parser.parse(source.encode("utf-8"))
 
-    def parse_file(self, path: str):
-        source = self.read_file(path)
+    def parse_file(self, ftp):
+        source = self.read_file(ftp)
         return self.parse_source(source)
 
 
 class QueryHandler:
     def __init__(self):
         self.file_parser = FileParser()
+        self.ftp = ftp
 
-    def get_tree(self, file):
-        tree = self.file_parser.parse_file(file)
+    def get_tree(self, ftp):
+        tree = self.file_parser.parse_file(ftp) #file
 
         print("Root node:", tree.root_node.type)
 
@@ -49,6 +50,6 @@ class QueryHandler:
 
 query_handler = QueryHandler()
 
-tree = query_handler.get_tree("test.py")
+tree = query_handler.get_tree(ftp)
 tree2 = query_handler.print_tree(tree.root_node)
 
