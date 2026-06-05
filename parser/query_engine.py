5403bd43435decc740384c1ce0a7f5396908c068
ZetaSploit <lazulilapis852@gmail.com>
2026-06-05 20:36:08 +0530

    still working on query_engine

--- parser/query_engine.py
+++ parser/query_engine.py
@@ -3,18 +3,15 @@ import tree_sitter_python as tspython
 import hashlib
 from treesitter_load import *
 
-query_handler = QueryHandler()
 
 if __name__ == "__main__":
 
-    def run_query(tree, query):
-        query = Query(PY_LANGUAGE, """(identifier) @id""")
+    def run_query(tree_obj, query):
+        query_string = "(identifier) @function.name"
+        query = Query(PY_LANGUAGE, query_string)
+        qcursor = QueryCursor()
 
-        cursor = QueryCursor(query)
+        for node, capture_name in qcursor.captures(query, tree.root_node):
 
-        for node in cursor.captures(tree.root_node):
-            #if node.encode("utf-8") == "md5":
-                #print("Found md5")
-            print(node)
 
-    run_query(query_handler.get_tree(ftp), "md5") 
+    
